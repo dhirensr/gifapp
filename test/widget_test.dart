d5a1d2d1931125b7ebe0d1bab498f0d300f53e38
dhirensr <dhirensr@gmail.com>
2020-07-06 17:13:58 +0200

    added sample test so that flutter test does not fails

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:gifapp/main.dart';
+import 'package:gifapp/main.dart';
 
 void main() {
-// keeping it empty for now
+  // keeping a sample test so all tests pass for now
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+    var textField = find.byType(Scaffold);
+
+    // Verify that our counter starts at 0.
+    expect(textField, findsOneWidget);
+  });
 }
